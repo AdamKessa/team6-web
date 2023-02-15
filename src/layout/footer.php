29d4272b4fdd33e6851c248c4fa6824d3d73418b
Stephen Yang <zhenyu22000@student.hamk.fi>
2023-02-15 11:30:28 +0200

    Just the whole we have done before.

---
+++ src/layout/footer.php
@@ -0,0 +1,80 @@
+<div class="row" id="footer1">
+              <div class="col-md-12 " >
+                <h3>WHY CHOOSE UZBEKOVKAT:</h3> 
+                <p style="font-size: 22px;">UZBEKOVKAT is the only international Uzbekistani food restorant in the world.
+                   We decided to take the initiative of making this restorant because we love and value
+                   Uzbek foods and delicacies and we would love to share them with the world through our fine salads, drinks and foods.</p>                   
+              </div> 
+              <div>
+                <h4><b>Order anywhere, anytime! download our App</h4> 
+                <a href=""><img class="AppStore" src="images/App Store Badge.png" alt="App Store" width="15px"></a> 
+                <a href=""><img class="AppStore" src="images/Google Play Store Badge.png" alt="Google Play Store" width="15px"></a> 
+              </div>           
+            </div>
+            <div class="container">
+              <div class="row" id="footer2">
+                <!-- <div class="col-md-3" id="">
+                  <p style="font-size: 22px;">Being a grain-farming country, rice, noodles, and breads are equally vital to Uzbek culture and cuisine. 
+                    In fact, plov or Uzbek rice pilaf is the country’s national dish and the most popular food in Uzbekistan.</p>                 
+                </div> -->
+                <div class="col  justify-content-center" id="Dubai" >
+                  <div id="myCarousel" class="carousel slide justify-content-center" data-ride="carousel" >                  
+                    <ol class="carousel-indicators">
+                      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                      <li data-target="#myCarousel" data-slide-to="1"></li>
+                      <li data-target="#myCarousel" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                      <div class="item active carousel-image">
+                        <img src="images/food/food7.jpg"  class="swipeimage" alt="Los Angeles" style="width:100%;">
+                      </div>
+                
+                      <div class="item carousel-image">
+                        <img src="images/food/food8.jpg" class="swipeimage"  alt="Chicago" style="width:100%;">
+                      </div>
+                    
+                      <div class="item carousel-image">
+                        <img src="images/food/food9.jpg" class="swipeimage1" alt="New york" style="width:100%;">
+                      </div>
+                    </div>            
+                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                      <span class="glyphicon glyphicon-chevron-left"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                      <span class="glyphicon glyphicon-chevron-right"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </div>
+                </div>     
+              </div>
+            </div>  
+            <div class="row" id="footer3">
+              <div class="col-md-6 " >
+                <p><b>LOCATION</b></p> 
+                <p>Rooftop-20th Floor, Grand Millennium Hotel, Marasi Drive Street,</p> 
+                <p>Business Bay, Dubai, UAE</p>
+                <p>Complimentary Valet Available</p>
+                <div>
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18906.129712753736!2d6.722624160288201!3d60.12672284414915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x463e997b1b6fc09d%3A0x6ee05405ec78a692!2sJ%C4%99zyk%20trola!5e0!3m2!1spl!2spl!4v1672239918130!5m2!1spl!2spl" width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+              </div>
+              <div class="col-md-6 " >
+                <p><b>OPENING HOURS</b></p> 
+                <p>For Booking & Reservations email us at hamk@uzbox.com </p>
+                <p>or call us at +671 00 11 3377</p>  
+                <p>Restaurant</p> 
+                <p>12 PM to 2 AM</p> 
+                <p>Lounge</p>
+                <p>6 PM to 3 AM</p>
+                <a href="#">FOLLOW US</a>
+              </div>               
+            </div>
+        </div>           
+         
+           
+  
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ src/layout/header.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>UZBEK FOOD</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Sofia+Sans:wght@200&display=swap" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="Home page.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="stylefood.css">
+        <link rel="icon" href="https://png.pngtree.com/png-vector/20220623/ourmid/pngtree-food-logo-png-image_5297921.png" >
+  </head>
+  <body>
+    
+        <div class="top">
+          <nav class="navbar navbar-expand-sm navbar-dark fs-3">
+            <div class="container-fluid">
+              <img class="login" src="images/R3.png" alt="Moroccan tomato salad" >
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link nav-link text-light" href="index.html"><p style="color: rgb(255, 106, 0);"><b>HOME</b></p></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link nav-link text-light" href="#">ABOUT US</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle nav-link text-light" href="#" role="button" data-bs-toggle="dropdown">MENU</a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item text-white bg-dark" href="food.html">FOOD</a></li>
+                      <li><a class="dropdown-item text-white bg-dark" href="salad.html">SALAD</a></li>
+                      <li><a class="dropdown-item text-white bg-dark" href="drinks.html">DRINK</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link nav-link text-light" href="#">GALLARY</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link nav-link text-light" href="#">RESERVATION</a>
+                  </li>  
+                  <li class="nav-item" id="navend">
+                    <a class="nav-link nav-link text-light" href="#">BLOG</a>
+                  </li>
+                  <li class="nav-item" id="facebook">
+                    <a class="nav-link nav-link text-light" href="#"><i class="bi bi-facebook"></i></a>
+                  </li> 
+                  <li class="nav-item" id="twitter">
+                    <a class="nav-link nav-link text-light" href="#"><i class="bi bi-twitter"></i></a>
+                  </li>                                   
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
