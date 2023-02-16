07b3979a6c9b31b40a8953cf76d0050c3f8b3ed5
soroush62 <112301194+soroush62@users.noreply.github.com>
2023-02-16 19:03:38 +0200

    some modifications

--- src/New project/Bookatable.php
+++ src/New project/Bookatable.php
@@ -8,14 +8,8 @@ include "../layout/header.php" ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $title  ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/js/bootstrap-datetimepicker.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/css/bootstrap-datetimepicker.min.css">
-        <style>
-             body {
+  <style>
+  body {
   background-image: url(images/salad.jpg);
   color: white;
   /* text-align: center;     */
@@ -32,7 +26,7 @@ include "../layout/header.php" ?>
   width: 100%;
   height: 100%;
 }
-h2 , h3 {
+h2 , h3 ,label {
   margin-left: 1%;
   color: white !important;
 }
@@ -49,6 +43,9 @@ h2 , h3 {
 }
 #tel {
     margin-left: 0;;
+}
+.w-50 {
+  height: 35px;
 }
 
  </style>        
@@ -68,37 +65,15 @@ h2 , h3 {
                   <option>4 persons</option>
                   <option>+5 persons</option>
                 </select>
-                <div class="">
-                  <div class="row">
-                    <div class='col-sm-6'>
-                      <div class="form-group">
-                        <!-- <span>Select time with Date</span> -->
-                        <div class='input-group date' id='datetimepicker2'>
-                          <input type='text' class="form-control"  placeholder="Select time with Date" />
-                          <span class="input-group-addon">
-                            <span class="glyphicon glyphicon-calendar"></span>
-                          </span>
-                        </div>
-                        <br/>
-                      
-                      </div>
-                    </div>
+                <div class="row">
+                  <label>Please choose Date and Time</label>
+                  <div class="col-xs-4" id="DateandTime">
+                    <input class="w-50 " type="date" name="bdate" placeholder="Date">
                   </div>
-                </div>
-                <script>
-                    $(function() {
-                  $('#datetimepicker1').datetimepicker({
-                            format: 'HH:mm'
-                  });
-                  
-                  $('#datetimepicker2').datetimepicker({
-                            format: 'MM/DD/YYYY HH:mm'
-                  });
-                  $('#datetimepicker3').datetimepicker({
-                      format: 'hh:mm A',
-                  });
-                });
-                </script>
+                  <div class="col-xs-4" id="DateandTime">
+                    <input  class="w-50" type="time" name="btime" placeholder="Time">
+                  </div>
+                </div><br><br>
                   <div class="col-xs-6">                  
                     <input class="form-control" type="text" form-select-lg  name="fname" placeholder="Fisrt Name                     required" required>
                   </div>
@@ -111,7 +86,7 @@ h2 , h3 {
                         <!-- <span class="input-group-text input-sm bg-secondary"><b>+358</b></span> -->
                         <button type="button" class="btn btn-primary "><b>+358</b></button>
                     </div>
-                    <input type="text" class="form-control w-75" aria-label="Amount (to the nearest dollar)" placeholder="Mobile Phone">
+                    <input type="text" class="form-control w-75" placeholder="Mobile Phone">
                 </div> <br><br>                 
                 <button type="submit" class="btn btn-primary btn-lg" value="Submit" name="submit">Submit</button>
   </form>
@@ -126,5 +101,5 @@ h2 , h3 {
 
 
 
-<!-- <?php include "../layout/footer.php" ?> -->
+<?php include "../layout/footer.php" ?>
          
