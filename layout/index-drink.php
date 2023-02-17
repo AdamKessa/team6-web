a1fd131bb1efd54268614b587eade2cac4bc51a0
soroush62 <112301194+soroush62@users.noreply.github.com>
2023-02-17 09:31:01 +0200

    some changes

--- layout/index-drink.php
+++ layout/index-drink.php
@@ -1,5 +1,5 @@
 <?php
-$title = "saladmenu";
+$title = "drinkmenu";
 include "header.php" ?>
 <!doctype html>
 <html lang="en">
@@ -8,7 +8,7 @@ include "header.php" ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $title  ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../CSS files/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Sofia+Sans:wght@200&display=swap" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
@@ -26,17 +26,16 @@ include "header.php" ?>
         </style>
         
   </head>
-  <body>  
+  <body>
     
-
-  <div class="">
-          
-             <div class="row" id="Linediv">
+        <div class="">
+       
+             <div class="row">
               <div class="col-md-4" id="firstcol">
                 <hr class="style1">
               </div>
               <div class="col-md-4">
-                <h1>Uzbek Salads</h1>
+                <h1>Uzbek Drinks</h1>
               </div>
               <div class="col-md-4" id="firstcol">
                 <hr class="style1">
@@ -44,30 +43,26 @@ include "header.php" ?>
              </div> 
             <div class="row" id="content">
                 <div class="col-md-3">
-                    <p class="mt-5 text-light">“Uzbekistan” salad, despite the name, isn’t part of traditional Uzbek 
-                      cuisine. Supposedly, it was invented in the 1950s in Moscow when Uzbek chefs tried to
-                       introduce Russians to their national cuisine for the first time. It combines Central Asian green radish 
-                      with the Russian love of mayonnaise – almost like an Uzbek version of the famous “Olivier” salad. It
-                       does look pretty “retro” – it reminds me of something you’d see in 1970s US cookbooks –
-                       but trust me, it’s delicious. The green radish is a refreshing complement to the fried onion, mayo,
-                       and oven-baked steak.Salads have their own unique taste and aroma.</p>
+                    <p class="mt-5 text-light">Uzbek tea secrets: Every country has its own tea tradition. 
+                      In Uzbekistan, this is a special culture. Every Uzbek citizen's day begins and ends with a sip of fragrant tea.
+                       After a hot breakfast in the morning, after a high-calorie lunch, dinner, or when guests arrive, tea is an irreplaceable part of a busy day</p>
                 </div>
               <div class="col-md-3">               
                 <div class="imagehover2 row mt-5">
-                  <img class="image2 rounded-circle" src="images/Moroccan tomato salad - 4$.jpg" alt="Moroccan tomato salad">
-                  <p>Garden salad </p>
-                  <h2> $4 </h2>
+                  <img class="image2 rounded-circle" src="images/uzb_tea.jpg" alt="Moroccan tomato salad">
+                  <p>Uzbek tea</p>
+                  <h2> $6 </h2>
                   <div class="overlay">
                     <div class="modal-header bg-white text-dark"> 
                       <div>
-                      <img src="images/Moroccan tomato salad - 4$.jpg" alt="Moroccan tomato salad" width="100px">  
+                      <img src="images/uzb_tea.jpg" alt="Moroccan tomato salad" width="100px">  
                       </div>
                       <div>                     
-                      <h4 class="modal-title  align-items-start">Moroccan tomato salad</h4>
+                      <h4 class="modal-title  align-items-start">Uzbek tea</h4>
                       </div>
                     </div>
                     <div class="modal-body  bg-white text-dark">                          
-                      <p>juicy tomatoes, fresh herbs, and traditional Moroccan spices.</p>
+                      <p style="color: gray;">Traditional Uzbek tea made with love</p>
                     </div>
                     <div class="modal-footer  bg-white text-dark justify-content-center">
                       <button type="button" class="btn btn-default justify-content-center" data-dismiss="modal"><p style="color: rgb(255, 115, 0); font-size: 20px;">order now</p></button>
@@ -76,20 +71,20 @@ include "header.php" ?>
                 </div>
 
                 <div class="imagehover2 row mt-5">
-                  <img class="image2 rounded-circle" src="images/Garden salad - 3$.jpg" alt="Garden salad">
-                  <p>Garden salad </p>
-                  <h2> $3 </h2>
+                  <img class="image2 rounded-circle" src="images/peache.jpg" alt="Garden salad">
+                  <p>Peach juice </p>
+                  <h2> $6 </h2>
                   <div class="overlay">
                     <div class="modal-header bg-white text-dark"> 
                       <div>
-                      <img src="images/Garden salad - 3$.jpg" alt="Garden salad" width="100px">  
+                      <img src="images/peache.jpg" alt="Garden salad" width="100px">  
                       </div>
                       <div>                     
-                      <h4 class="modal-title  align-items-start">Garden salad</h4>
+                      <h4 class="modal-title  align-items-start">Peach juice</h4>
                       </div>
                     </div>
                     <div class="modal-body  bg-white text-dark">                          
-                      <p >red onion, green onion, cucumber, cherry tomatoes, radishes and carrots</p>
+                      <p style="color: gray;">Fresh peach juice</p>
                     </div>
                     <div class="modal-footer  bg-white text-dark justify-content-center">
                       <button type="button" class="btn btn-default justify-content-center" data-dismiss="modal"><p style="color: rgb(255, 115, 0); font-size: 20px;">order now</p></button>
@@ -100,20 +95,20 @@ include "header.php" ?>
 
               <div class="col-md-3">                
                 <div class="imagehover2 row mt-5">
-                  <img class="image2 rounded-circle" src="images/Caesar salad - 6$.jpg" alt="Caesar salad">
-                  <p>Garden salad </p>
-                  <h2> $3 </h2>
+                  <img class="image2 rounded-circle" src="images/kiwiberry.jpg" alt="Caesar salad">
+                  <p> Kiwi Berry</p>
+                  <h2> $6 </h2>
                   <div class="overlay">
                     <div class="modal-header bg-white text-dark"> 
                       <div>
-                      <img src="images/Caesar salad - 6$.jpg" alt="Caesar salad" width="100px">  
+                      <img src="images/kiwiberry.jpg" alt="Caesar salad" width="100px">  
                       </div>
                       <div>                     
-                      <h4 class="modal-title  align-items-start">Caesar salad</h4>
+                      <h4 class="modal-title  align-items-start">Kiwi Berry</h4>
                       </div>
                     </div>
                     <div class="modal-body  bg-white text-dark">                          
-                      <p>lettuce, Parmesan cheese, Crisp croutons,Caesar salad dressing</p>
+                      <p style="color: gray;">Fresh kiwi berry juice</p>
                     </div>
                     <div class="modal-footer  bg-white text-dark justify-content-center">
                       <button type="button" class="btn btn-default justify-content-center" data-dismiss="modal"><p style="color: rgb(255, 115, 0); font-size: 20px;">order now</p></button>
@@ -122,20 +117,20 @@ include "header.php" ?>
                 </div>
 
                 <div class="imagehover2 row mt-5">
-                  <img class="image2 rounded-circle" src="images/Spicy Squid Salad2 6$.jpg" alt="Spicy Squid Salad">
-                  <p>Spicy Squid salad</p>
+                  <img class="image2 rounded-circle" src="images/tomatoe.jpeg" alt="Spicy Squid Salad">
+                  <p>Tomato juice</p>
                   <h2> $6 </h2>
                   <div class="overlay">
                     <div class="modal-header bg-white text-dark"> 
                       <div>
-                      <img src="images/Spicy Squid Salad2 6$.jpg" alt="Spicy Squid Salad" width="100px">  
+                      <img src="images/tomatoe.jpeg" alt="Spicy Squid Salad" width="100px">  
                       </div>
                       <div>                     
-                      <h4 class="modal-title  align-items-start">Spicy Squid Salad</h4>
+                      <h4 class="modal-title  align-items-start">Tomato juice</h4>
                       </div>
                     </div>
                     <div class="modal-body  bg-white text-dark">                          
-                      <p>a spicy, sweet dish made with boiled squid and fresh vegetables</p>
+                      <p style="color: gray;">fresh tomatoe juice</p>
                     </div>
                     <div class="modal-footer  bg-white text-dark justify-content-center">
                       <button type="button" class="btn btn-default justify-content-center" data-dismiss="modal"><p style="color: rgb(255, 115, 0); font-size: 20px;">order now</p></button>
@@ -146,20 +141,20 @@ include "header.php" ?>
               
               <div class="col-md-3"> 
                 <div class="imagehover2 row mt-5">
-                  <img class="image2 rounded-circle" src="images/shanghai salad.jpg" alt="shanghai salad">
-                  <p>shanghai salad</p>
-                  <h2> $5 </h2>
+                  <img class="image2 rounded-circle" src="images/orange.jpg" alt="shanghai salad">
+                  <p>Orange juice</p>
+                  <h2> $4 </h2>
                   <div class="overlay">
                     <div class="modal-header bg-white text-dark"> 
                       <div>
-                      <img src="images/shanghai salad.jpg" alt="shanghai salad" width="100px">  
+                      <img src="images/orange.jpg" alt="shanghai salad" width="100px">  
                       </div>
                       <div>                     
-                      <h4 class="modal-title  align-items-start">shanghai salad</h4>
+                      <h4 class="modal-title  align-items-start">orange Juice</h4>
                       </div>
                     </div>
                     <div class="modal-body  bg-white text-dark">                          
-                      <p>chicken, peppers, oranges, onions, wonton strips and cashews</p>
+                      <p style="color: gray;">chicken, peppers, oranges, onions, wonton strips and cashews</p>
                     </div>
                     <div class="modal-footer  bg-white text-dark justify-content-center">
                       <button type="button" class="btn btn-default justify-content-center" data-dismiss="modal"><p style="color: rgb(255, 115, 0); font-size: 20px;">order now</p></button>
@@ -170,20 +165,20 @@ include "header.php" ?>
                 
 
                 <div class="imagehover2 row mt-5">
-                  <img class="image2 rounded-circle" src="images/cobb salad 3$.jpg" alt="cobb salad">
-                  <p>cobb salad</p>
+                  <img class="image2 rounded-circle" src="images/apple.jpg" alt="cobb salad">
+                  <p>Apple juice</p>
                   <h2> $6 </h2>
                   <div class="overlay">
                     <div class="modal-header bg-white text-dark"> 
                       <div>
-                      <img src="images/cobb salad 3$.jpg" alt="cobb salad" width="100px">  
+                      <img src="images/apple.jpg" alt="cobb salad" width="100px">  
                       </div>
                       <div>                     
-                      <h4 class="modal-title  align-items-start">cobb salad</h4>
+                      <h4 class="modal-title  align-items-start"> Apple juice</h4>
                       </div>
                     </div>
                     <div class="modal-body  bg-white text-dark">                          
-                      <p>crispy bacon, hard-boiled eggs, chicken breast and creamy avocado</p>
+                      <p style="color: gray;">fresh apple juice</p>
                     </div>
                     <div class="modal-footer  bg-white text-dark justify-content-center">
                       <button type="button" class="btn btn-default justify-content-center" data-dismiss="modal"><p style="color: rgb(255, 115, 0); font-size: 20px;">order now</p></button>
@@ -200,7 +195,7 @@ include "header.php" ?>
             <div class="row" id="footer1">
               <div class="col-md-12 " >
                 <h3>WHY CHOOSE UZBEKOVKAT:</h3> 
-                <p style="font-size: 22px;">UZBEKOVKAT is the only international Uzbekistani food restorant in the world.
+                <p style="font-size: 17px;">UZBEKOVKAT is the only international Uzbekistani food restorant in the world.
                    We decided to take the initiative of making this restorant because we love and value
                    Uzbek foods and delicacies and we would love to share them with the world through our fine salads, drinks and foods.</p>                   
               </div>
@@ -211,7 +206,8 @@ include "header.php" ?>
               </div>              
             </div>
             <div class="container">
-              <div class="row" id="footer2">                
+              <div class="row" id="footer2">
+                         
                 <div class="col  justify-content-center" id="Dubai" style="width: 270px; height: 300px;">
                   <div id="myCarousel" class="carousel slide justify-content-center" data-ride="carousel" >                  
                     <ol class="carousel-indicators">
@@ -221,15 +217,15 @@ include "header.php" ?>
                     </ol>
                     <div class="carousel-inner">
                       <div class="item active carousel-image">
-                        <img src="images/salad5.jpg" alt="Los Angeles" style="width:100%;">
+                        <img src="images/strawberry_cocktail.jpg" alt="Los Angeles" style="width:100%;">
                       </div>
                 
                       <div class="item carousel-image">
-                        <img src="images/salad6.jpg" alt="Chicago" style="width:100%;">
+                        <img src="images/sumalyak.jpg" alt="Chicago" style="width:100%;">
                       </div>
                     
                       <div class="item carousel-image">
-                        <img src="images/salad7.jpg" alt="New york" style="width:100%;">
+                        <img src="images/strawbery.jpg" alt="New york" style="width:100%;">
                       </div>
                     </div>            
                     <a class="left carousel-control" href="#myCarousel" data-slide="prev">
@@ -246,9 +242,5 @@ include "header.php" ?>
             </div>  
             
         </div>           
-          
-          
-  
-    
-  
-<?php include "footer.php" ?>
+             
+            <?php include "footer.php" ?>
