d369f6136c93e95925ddbe20e2c7335fe9e51264
soroush62 <112301194+soroush62@users.noreply.github.com>
2023-02-18 18:52:06 +0200

    create external css for bookatable

--- WEbProgrammingProject/PHP_files/Bookatable.php
+++ WEbProgrammingProject/PHP_files/Bookatable.php
@@ -8,7 +8,7 @@ include "header.php" ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $title  ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="../CSS_files/Bookatable.css">       
+    <link rel="stylesheet" href="../CSS_files/bookatable.css">       
   </head>
   <form action="" method="post">
      <body>   
