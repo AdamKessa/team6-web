1108010bece4941d3f7fb1ce5189ecbf888a9247
Stephen Yang <zhenyu22000@student.hamk.fi>
2023-02-22 11:27:53 +0200

    file structure for the docker
    fix file structure for the docker, And some header and footer changes.

---
+++ src/layout/PHP_files/header.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <title> <?php echo $title ?> </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    
+
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&amp;family=Sofia+Sans:wght@200&amp;display=swap"
+        rel="stylesheet">
+    
+
+    <link rel="icon" href="https://png.pngtree.com/png-vector/20220623/ourmid/pngtree-food-logo-png-image_5297921.png" >
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Sofia+Sans:wght@200&display=swap" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+   
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="<?php echo $css ?>">
+        
+
+ 
+</head>
+
+<body>
+   <div>
+
+       <div class="Home-page">
+   
+           <div class="cont">
+   
+               
+               <nav>
+                   <ul >
+                    
+                    <img class="" src="layout/images/R3.png" alt="Moroccan tomato salad" width="100px" height="100px">    
+                   
+                        
+                       <li><a class="active"  style="font-size:17px ;"  href="home.php">HOME</a></li>
+                       <div>
+                       <div class="nav-item dropdown">
+                          <a href="#" class="nav-link dropdown-toggle text-light" data-bs-toggle="dropdown">MENU</a>
+                          <div class="dropdown-menu">
+                              <a href="index-food.php" class="dropdown-item text-white bg-dark">FOOD</a>
+                              <a href="index-salad.php" class="dropdown-item text-white bg-dark">SALAD</a>
+                              <a href="index-drinks.php" class="dropdown-item text-white bg-dark">DRINK</a>
+                          </div>
+                      </div>
+                       </div>
+                       
+                       
+                       <li><a href="#">GALLERY</a></li>
+                       <li><a href="#">ABOUT US</a></li>
+                       <li><a href="#">LOCATION</a></li>
+                       <li class="blog"> <a href="">BLOG</a> </li>
+   
+                       <!--     Add icon library    -->
+                       
+                       <li class="face">
+                           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">   
+                           <a href="#" class="fa fa-facebook"></a>
+                           <a href="#" class="fa fa-twitter"></a>
+                       </li>
+                       <li class="face"> </a> </li>
+   
+                   </ul>
+               </nav>
+               </div>
+    
+   
+       
