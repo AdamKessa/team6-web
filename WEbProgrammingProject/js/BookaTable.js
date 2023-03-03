efede9d068bfd22b96fea5234972534f47517351
soroush62 <112301194+soroush62@users.noreply.github.com>
2023-03-03 08:19:51 +0200

    add some functions

--- WEbProgrammingProject/js/BookaTable.js
+++ WEbProgrammingProject/js/BookaTable.js
@@ -7,22 +7,23 @@ function Book(){
       alert("Name can't be blank");  
       return false;  
     } 
-     //validating Last name 
-    let llname=document.myform.lname.value;     
-    if (llname==null || llname==""){  
-      alert("Last name can't be blank");  
-      return false;  
-    }
-    //validating phonenumber 
-    let Phone=document.myform.phonenumber.value;     
-    if (Phone==null || Phone==""){  
-      alert("Phonenumber can't be blank");  
-      return false;  
-    }      
-    //validating email 
-    let email=document.myform.email.value;
-    if (email==null || email==""){  
-        alert("Email can't be blank");  
-        return false;  
-      }
+    //  //validating Last name 
+    // let llname=document.myform.lname.value;     
+    // if (llname==null || llname==""){  
+    //   alert("Last name can't be blank");  
+    //   return false;  
+    // }
+    // //validating phonenumber 
+    // let Phone=document.myform.phonenumber.value;     
+    // if (Phone==null || Phone==""){  
+    //   alert("Phonenumber can't be blank");  
+    //   return false;  
+    // }      
+    // //validating email 
+    // let email=document.myform.email.value;
+    // if (email==null || email==""){  
+    //     alert("Email can't be blank");  
+    //     return false;  
+    //   }
+    
     }
