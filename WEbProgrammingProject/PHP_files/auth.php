2e534e6e0770684ba8dc369a4177569a0c6ad611
Rorsa-All <my.journey.h@gmail.com>
2023-03-05 12:09:37 +0300

    new login

--- WEbProgrammingProject/PHP_files/auth.php
+++ WEbProgrammingProject/PHP_files/auth.php
@@ -1,24 +1,16 @@
 <?php
-$login=filter_var(trim($_POST['login']), FILTER_SANITIZE_STRING);
-$pass=filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING);
+session_start();
+include("db_connect.php");
 
-$pass = md5($pass."dwq12e1290");
-
-$mysql=new mysqli('db','asror','asror2003','team6-web');
-
-$result=$mysql->query("SELECT * FROM `users` WHERE `login`='$login' AND `pass`='$pass' ");
-$user = $result->fetch_assoc();
-if(is_countable($user)==0)
+$email=$_POST['email'];
+$password=$_POST['password'];
+$md5_password = md5($password);
+$query=mysqli_query($db,"SELECT * FROM `users` WHERE `email`='$email' AND `password`='$md5_password'");
+if(mysqli_num_rows($query)!==0)
 {
-    echo"no such user found";
-    exit();
+    $_SESSION['user']=['nick'=>$email];
+    header("Location: user.php");
 }
-
- setcookie('user',$user['name'],time()+3600,"/");
-
-$mysql->close();
-
-header("Location: index.php");
-// exit();
-?>
-
+else{
+echo "Invalid login and/or password";
+}
--- WEbProgrammingProject/PHP_files/sign_up.php
+++ WEbProgrammingProject/PHP_files/sign_up.php
@@ -1,27 +1,26 @@
-
 <?php 
-$title = "Log in";
+$title = "Sign up";
 include "header.php";?>
 
 <div class="log_sign">
 
-    <form action="auth.php" onsubmit="return crud()" name="form2" method="post">
+    <form action="registration.php" onsubmit="return crud()" name="form2" method="post">
     
         <label for="Email1">Email</label>
         <input type="text" class="form-control" name="email" id="email" placeholder="Enter Email" required><br>
-    
+        
+        <label for="Name">Name</label>
+        <input type="text" class="form-control" name="name" id="name" placeholder="Enter Name" required><br>
+        
         <label for="Password1">Password</label>
         <input type="password" class="form-control" name="password" id="password" placeholder="Password" required><br>
-        <button class="btn btn-success" type="submit">Log in</button>
+        <button class="btn btn-success" type="submit">Sign up</button>
         <!-- <p class="btn btn-success"><input class="button" name="login"type= "submit" value="Log In"></p> -->
-	<p class="text" style="color: white ;">Not registered yet?<a href= "sign_up.php">Registration</a>!</p>
+
    </form>
-<?php
-
-?>   
+    
+    
     </form>
-   
-
 </div>
 
 <?php include "footer.php" ?>
