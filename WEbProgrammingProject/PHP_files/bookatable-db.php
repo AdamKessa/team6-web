73a171c07ec6d8fca9a744d67c9a2c680fb75628
soroush62 <112301194+soroush62@users.noreply.github.com>
2023-03-06 12:58:12 +0200

    modification

--- WEbProgrammingProject/PHP_files/bookatable-db.php
+++ WEbProgrammingProject/PHP_files/bookatable-db.php
@@ -1,8 +1,8 @@
 <?php
 $servername="db";
-$username="bookatable";
-$password="BookaTable";
-$dbname="bookatable";
+$username="team6";
+$password="team6";
+$dbname="team6-web";
 $conn=new mysqli ($servername, $username, $password, $dbname);
 if($conn->connect_error){
     die("connection failed:" . $conn->connect_error);
